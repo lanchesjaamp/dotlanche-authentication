8f7d835e57fd31d04119b788a6b2b92320938ef1
xJohnNascimento <xjohn.nascimento@gmail.com>
2024-10-01 10:57:42 -0300

    initial commit

---
+++ src/DotLancheAuthentication/DotLancheAuthentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.Lambda.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotLancheAuthentication
+{
+    [LambdaStartup]
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+    }
+}
