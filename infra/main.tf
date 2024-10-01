6733ea89c89970e3c037d361189c97d4cc74779b
xJohnNascimento <xjohn.nascimento@gmail.com>
2024-10-01 16:46:59 -0300

    .

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,3 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-bucket-test"
 }
