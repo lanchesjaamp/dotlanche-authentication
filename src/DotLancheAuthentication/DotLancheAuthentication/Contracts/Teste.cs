c94cae386128efdb6a7ab56e66e5535a35d6b90f
xJohnNascimento <xjohn.nascimento@gmail.com>
2024-10-01 21:03:35 -0300

    .

---
+++ src/DotLancheAuthentication/DotLancheAuthentication/Contracts/Teste.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotLancheAuthentication.Contracts
+{
+    public class Teste
+    {
+        public object Test { get; set; }
+    }
+}
--- src/DotlancheAuthentication/Core/Common/BaseResult.cs
+++
@@ -1,8 +0,0 @@
-namespace DotlancheAuthentication.Core.Common;
-
-public class BaseResult
-{
-    public bool Success { get; set; }
-
-    public string? Message { get; set; }
-}
